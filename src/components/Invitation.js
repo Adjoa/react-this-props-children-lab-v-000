5fb520fbea5f41dc2f0e4caa9d036ec46fe691de
Adjoa <adjoa.darien@gmail.com>
2018-05-18 20:34:45 +0000

    Done.

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,16 @@
-// Code Invitation Component Here
+import React from 'react';
+import { render } from 'react-dom';
+// import ReactDOM from 'react-dom';
+
+class Invitation extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        {this.props.children}
+      </div>
+      );
+  };
+};
+
+export default Invitation;
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,21 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+import { render } from 'react-dom';
+// import ReactDOM from 'react-dom';
+
+class ThemedDecorations extends React.Component {
+  render() {
+    const themedDivs = React.Children.map(this.props.children, child => {
+      return (
+        <div className={this.props.theme}>{child}</div> 
+      );
+    })
+    
+    return (
+      <div>
+        {themedDivs}
+      </div>
+    );
+  }
+};
+
+export default ThemedDecorations;
